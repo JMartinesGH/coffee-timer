26eacb3cd81071476f3808e2c3809b28a0a910c1
Jamie Martines <dev.jpm@gmail.com>
2021-01-31 18:38:11 -0500

    temporary solution for the select/option for minutes and seconds, will convert to component to save on mem/performance

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
+
 export default class Recipe extends React.Component { 
   state = {
     name: '',
     vessel: '',
     coffeeWeight: '',
     waterWeight: '',
-    brewTime: '',
+    brewTimeMinutes: 0,
+    brewTimeSeconds: 0,
     bloom: {
       isActive: false,
-      bloomTime: ''
+      bloomTime: 0
     }
   }
   handleSubmit = (event) => {
@@ -24,47 +26,48 @@ export default class Recipe extends React.Component {
     })
   }
   render() { 
+    let selectMinute = []
+    for (let i = 0; i < 11; i++) { 
+      selectMinute.push(<option key={i} value={i}>{i}</option>)
+    }
+
     return (
       <div>
         <h1>Create a Recipe</h1>
         <form className='recipe' onSubmit={this.handleSubmit}>
-            <label htmlFor='username' className='recipe-label'>
-              Nickname
-            </label>
-            <div className='row recipe-inputs'>
-              <input
-                type='text'
-                id='name'
-                name='name'
-                className={`input-name`}
-                placeholder='Recipe Name'
-                autoComplete='off'
-                value={this.state.name}
-                onChange={this.handelChange}
-              />
-            </div>
-            {/* <div className='row recipe-inputs'>
-              <input
-                type='text'
-                id='name'
-                name='name'
-                className={`input-name`}
-                placeholder='Recipe Name'
-                autoComplete='off'
-                value={this.state.name}
-                onChange={this.handelChange}
-              />
-            </div> */}
-            <div>
-              <button
-                  className={`btn `}
-                  type='submit'
-                  disabled={!this.state.name}
-                >
-                Add Recipe
-              </button>
-            </div>
-          </form>
+          <label htmlFor='username' className='recipe-label'>
+            Nickname
+          </label>
+          <div className='row recipe-inputs'>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              className={`input-name`}
+              placeholder='Recipe Name'
+              autoComplete='off'
+              value={this.state.name}
+              onChange={this.handelChange}
+            />
+          </div>
+          <div>
+          <label htmlFor='username' className='recipe-label'>
+            Minutes
+          </label>
+          <select name={this.state.brewTimeMinutes} onChange={this.handleChange}>
+          { selectMinute.map((item)=>item)}
+          </select>
+          </div>
+          <div>
+            <button
+                className={`btn `}
+                type='submit'
+                disabled={!this.state.name}
+              >
+              Add Recipe
+            </button>
+          </div>
+        </form>
       </div>
     )
   }
