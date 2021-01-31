4f373ece737620cd7ee1e16030b9e2c81325ae53
Jamie Martines <dev.jpm@gmail.com>
2021-01-31 16:20:09 -0500

    some app clean-up from the create react app files, basic home view started

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+function RecipeList({ recipes }) { 
+  return (
+    <ul>
+      {recipes.map(recipe => (
+        <li key={recipe.id}>{ recipe.name }</li>
+      ))}
+    </ul>
+  )
+}
+
 export default class Popular extends React.Component { 
+  state = {
+    recipes: [
+      {
+        name: 'test recipe',
+        id: '1'
+      }
+    ],
+  }
   render() { 
+    const { recipes } = this.state 
+
     return (
       <div>
         <h1>Coffee Timer</h1>
+        {recipes && <RecipeList recipes={recipes}/>}
+        <button>Add Recipe</button>
       </div>
     )
   }
