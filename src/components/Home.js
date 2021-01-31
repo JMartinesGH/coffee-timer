351db961feb17ce66f52635e6c0e5bf37949f7da
Jamie Martines <dev.jpm@gmail.com>
2021-01-31 14:54:13 -0500

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Popular extends React.Component { 
+  render() { 
+    return (
+      <div>
+        <h1>Coffee Timer</h1>
+      </div>
+    )
+  }
+}
