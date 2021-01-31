de4034a7a4b77035259fe64ee583b1128f530f98
Jamie Martines <dev.jpm@gmail.com>
2021-01-31 17:11:54 -0500

    prep for the form on recipe/add

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,57 @@
 import React from 'react'
 
 export default class Recipe extends React.Component { 
+  state = {
+    name: '',
+    vessel: '',
+    coffeeWeight: '',
+    waterWeight: '',
+    brewTime: '',
+    bloom: {
+      isActive: false,
+      bloomTime: ''
+    }
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log('submitted')
+  }
+  handleNameChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
   render() { 
     return (
-      <h1>Create a Recipe</h1>
+      <div>
+        <h1>Create a Recipe</h1>
+        <form className='recipe' onSubmit={this.handleSubmit}>
+            <label htmlFor='username' className='recipe-label'>
+              Nickname
+            </label>
+            <div className='row recipe-inputs'>
+              <input
+                type='text'
+                id='username'
+                className={`input-name`}
+                placeholder='Recipe Name'
+                autoComplete='off'
+                value={this.state.name}
+                onChange={this.handleNameChange}
+              />
+          </div>
+          <div>
+            <button
+                className={`btn `}
+                type='submit'
+                disabled={!this.state.name}
+              >
+                Add Recipe
+              </button>
+          </div>
+          </form>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -26,4 +26,3 @@ ReactDOM.render(
   <App/>,
   document.getElementById('root')
 );
-
